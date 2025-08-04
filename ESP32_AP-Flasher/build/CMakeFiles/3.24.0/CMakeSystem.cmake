aabab7e752693d467909132ccad7117b5c7d6de9
faztrick <faztrick@gmail.com>
2025-08-04 22:00:02 +0400

    update 140 files and delete 7 files

--- ESP32_AP-Flasher/build/CMakeFiles/3.24.0/CMakeSystem.cmake
+++ ESP32_AP-Flasher/build/CMakeFiles/3.24.0/CMakeSystem.cmake
@@ -3,7 +3,7 @@ set(CMAKE_HOST_SYSTEM_NAME "Windows")
 set(CMAKE_HOST_SYSTEM_VERSION "10.0.27913")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "AMD64")
 
-include("C:/Users/faztrick/esp/v5.3.1/esp-idf/tools/cmake/toolchain-esp32s3.cmake")
+include("C:/Users/faztrick/esp/v5.3.1/esp-idf/tools/cmake/toolchain-esp32c6.cmake")
 
 set(CMAKE_SYSTEM "Generic")
 set(CMAKE_SYSTEM_NAME "Generic")
--- ESP32_AP-Flasher/build/esp-idf/esp_hw_support/cmake_install.cmake
+++ ESP32_AP-Flasher/build/esp-idf/esp_hw_support/cmake_install.cmake
@@ -34,12 +34,12 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/faztrick/.espressif/tools/xtensa-esp-elf/esp-13.2.0_20240530/xtensa-esp-elf/bin/xtensa-esp32s3-elf-objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/faztrick/.espressif/tools/riscv32-esp-elf/esp-13.2.0_20240530/riscv32-esp-elf/bin/riscv32-esp-elf-objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for the subdirectory.
-  include("D:/projects/esp/OpenEPaperLink/ESP32_AP-Flasher/build/esp-idf/esp_hw_support/port/esp32s3/cmake_install.cmake")
+  include("D:/projects/esp/OpenEPaperLink/ESP32_AP-Flasher/build/esp-idf/esp_hw_support/port/esp32c6/cmake_install.cmake")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
